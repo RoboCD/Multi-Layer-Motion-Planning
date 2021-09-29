4cab8109ba730dd3c2f76cf61d110af2c6b2adf6
RoboCD <chrisjd91@gmail.com>
2021-09-28 20:42:47 -0400

    Updated obstruction environment grid map tutorial

--- ROS_WS/src/obstruction_environment/src/obstruction_environment_node.cpp
+++ ROS_WS/src/obstruction_environment/src/obstruction_environment_node.cpp
@@ -13,23 +13,26 @@ int main(int argc, char **argv)
 
     // Create grid map.
     grid_map::GridMap map({"obstacleMap"});
+
     map.setGeometry(grid_map::Length(48,48),1, grid_map::Position(0,0));
     map.setFrameId("map");
 
     ros::Duration longDuration(10.0);
     ros::Duration duration(1.0);
+
     grid_map::Position testPos(1,1);
     map.setTimestamp(ros::Time::now().toNSec());
 
+    // Initial publish
     grid_map::GridMapRosConverter::toMessage(map, message);
     publisher.publish(message);
     ROS_INFO("Message Published");
-    longDuration.sleep();
+    duration.sleep();
 
     // for (int i = 0; i<20; i++){
 
     
-    map.atPosition("obstacleMap",testPos) = 0.0;
+    // map.atPosition("obstacleMap",testPos) = 0.0;
     duration.sleep();
     map.atPosition("obstacleMap",testPos) = 1.0;
 
@@ -39,6 +42,7 @@ int main(int argc, char **argv)
     map.setTimestamp(ros::Time::now().toNSec());
     grid_map::GridMapRosConverter::toMessage(map, message);
     publisher.publish(message);
+
     ROS_INFO("Message Published");
     duration.sleep();
     // }
